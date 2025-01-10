d21b2e6d1bf9081471793522023878cc1c2b4578
sarav929 <ec2325508@edinburghcollege.ac.uk>
2025-01-10 17:31:04 +0000

    update read.php for admin management

--- admin/read.php
+++ admin/read.php
@@ -20,18 +20,22 @@
                 $item_img = !empty($row['item_img']) ? $row['item_img'] : 'placeholder.jpg'; 
                 $item_name = !empty($row['item_name']) ? $row['item_name'] : 'No Name';
                 $item_desc = !empty($row['item_desc']) ? $row['item_desc'] : 'No description available.';
-                $item_price = !empty($row['item_price']) ? '£ ' . $row['item_price'] : 'Price not set';
+                $item_price = !empty($row['item_price']) ? '£ '. $row['item_price'] : 'Price not set';
+                $item_brand = !empty($row['item_brand']) ? $row['item_brand'] : 'Brand not set';
+                $item_notes = !empty($row['item_notes']) ? $row['item_notes'] : 'Notes are not set';
+                $item_type = !empty($row['item_type']) ? $row['item_type'] : 'Type is not set';
+                $item_ml = !empty($row['item_ml']) ? $row['item_ml'] . 'ml' : 'Capacity is not set';
 
                 echo '
                 <div class="col-md-3 d-flex justify-content-center mt-4 p-3 text-center">
                     <div class="card" style="width: 18rem;">
-                        <img src="../assets/img/' . htmlspecialchars($item_img) . '" class="card-img-top" alt="Product Image">
+                        <img src="../assets/img/' . htmlspecialchars($item_img) . '.png" class="card-img-top" alt="Product Image">
                         <div class="card-body">
                             <h5 class="card-title text-center">' . htmlspecialchars($item_name) . '</h5>
-                            <p class="card-text">' . htmlspecialchars($item_desc) . '</p>
+                            <p class="card-text">by ' . htmlspecialchars($item_brand) . '</p>
                         </div>
                         <ul class="list-group list-group-flush text-center">
-                            <li class="list-group-item"><p class="d-flex justify-content-center align-items-center">' . htmlspecialchars($item_price) . '</p></li>
+                            <li class="list-group-item"><p class="d-flex justify-content-center align-items-center">' . htmlspecialchars($item_price) . ' - ' . htmlspecialchars($item_ml) . '</p></li>
                             <li class="list-group-item">
                                 <a class="btn btn-dark btn-lg btn-block" href="../admin/update.php?id=' . $row['item_id'] . '">Update</a>
                             </li>
