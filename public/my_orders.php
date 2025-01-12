3a89d6e925e9168cc7d9219e01aeb0d131ff0094
sarav929 <ec2325508@edinburghcollege.ac.uk>
2025-01-12 11:19:31 +0000

    create SQL query to retrieve order details

--- public/my_orders.php
+++ public/my_orders.php
@@ -24,35 +24,35 @@
     # 1. Connect to the database
     require('../config/connect.php');
 
-    # 2. Retrieve the orders 
-    $q = "SELECT * FROM orders WHERE user_id = " . $_SESSION['user_id'];
+    # 2. Retrieve the logged in user's orders with contents 
+
+    $user_id = (int)$_SESSION['user_id'];
+
+    $q = "SELECT 
+        orders.order_id, 
+        orders.order_date, 
+        orders.total, 
+        order_contents.item_id, 
+        products.item_name, 
+        products.item_price, 
+        products.item_img,
+        order_contents.quantity, 
+        (products.item_price * order_contents.quantity) AS item_total 
+    FROM orders 
+    JOIN order_contents ON orders.order_id = order_contents.order_id 
+    JOIN products ON order_contents.item_id = products.item_id 
+    WHERE orders.user_id = $user_id 
+    ORDER BY orders.order_id, orders.order_date DESC";
+
+    echo '<div class="container text-center mt-5"> 
+        <h2>My Orders</h2>
+    </div>';
+
     $r = mysqli_query($link, $q);
+    
+    $orders = [];
 
-    # If there are any orders in the table: display them
-    if (mysqli_num_rows($r) > 0) {
-
-        while ($row = mysqli_fetch_array($r, MYSQLI_ASSOC)) {
-            // Check if all fields are present
-            $order_id = !empty($row['order_id']) ? $row['order_id'] : 'N/A'; 
-            $order_total = !empty($row['total']) ? $row['total'] : 'N/A';
-            $order_date = !empty($row['order_date']) ? $row['order_date'] : 'N/A';
-
-            echo '<div> 
-            <p> Order #' . $order_id . '</p>
-            <p> Order total: ' . $order_total . '</p>
-            <p> Date: ' . $order_date . '</p>
-            <p> Status: Confirmed </p>
-            </div>';
-        }
-
-        # Close the database connection
-        mysqli_close($link);
-
-    } else { 
-        # else: print out that the table is empty 
-        echo '<h2 class="text-center mt-4 p-3">No orders to display.</p>'; 
-    }
-
+    
 
     include '../includes/footer.php'; ?>
     
