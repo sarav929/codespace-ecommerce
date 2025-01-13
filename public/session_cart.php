4e63acaec227224c01934aec2fcb89afff702c8d
sarav929 <ec2325508@edinburghcollege.ac.uk>
2025-01-13 15:41:28 +0000

    fix styling for filtering

--- public/session_cart.php
+++ public/session_cart.php
@@ -69,92 +69,95 @@
         $brands_list = mysqli_query($link, "SELECT DISTINCT item_brand FROM products");
 
         # Page Content
-        echo "<div class='container text-center mt-5'> 
-            <h2>Fragrances</h2>
-            <p class='font-italic'>Explore our selection of eaux and extraits de parfum</p> 
-        </div>
+        echo "<div class='container m-auto'>
+            <div class='mt-5 text-center'> 
+                <h2>Fragrances</h2>
+                <p class='font-italic'>Explore our selection of eaux and extraits de parfum</p> 
+            </div>";
 
-        <div class='container d-flex justify-content-between'>
+            # container for dropdowns
+            echo "<div class='container d-flex align-items-center mt-4 text-right'>
+                
+                <div class='d-flex align-items-center'> 
+                <img class='mr-3' src='../assets/icons/filter.png' style='width:1.2rem;'>";
+                
+                    # Dropdown for brands
+                    echo '<div class="dropdown mr-3">
+                        <a class="btn btn-light dropdown-toggle" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            Brand
+                        </a>
+                        <div class="dropdown-menu" aria-labelledby="dropdownMenuLink">
+                        <a class="dropdown-item" href="../public/session_cart.php?&sort=' . $sort . '">All</a>
+                        <div class="dropdown-divider"></div>';
+                    while ($row = mysqli_fetch_array($brands_list, MYSQLI_ASSOC)) {
+                        echo '<a class="dropdown-item" href="../public/session_cart.php?brand=' . $row['item_brand'] . '">' . $row['item_brand'] . '</a>';
+                    }
+                    echo '</div></div>';
 
-            <div class='container d-flex justify-content-start'>";
+                    # Dropdown for types
+                    echo '<div class="dropdown mr-3">
+                        <a class="btn btn-light dropdown-toggle" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            Type
+                        </a>
+                        <div class="dropdown-menu" aria-labelledby="dropdownMenuLink">
+                        <a class="dropdown-item" href="../public/session_cart.php?&sort=' . $sort . '">All</a>
+                        <div class="dropdown-divider"></div>';
+                    foreach ($types as $type) {
+                        echo '<a class="dropdown-item" href="../public/session_cart.php?type=' . $type . '&sort=' . $sort . '">' . $type . '</a>';
+                    }
+                    echo '</div></div>
+                </div>';
 
-                # Dropdown for brands
-                echo '<div class="dropdown mr-5 col">
-                    <a class="btn btn-light dropdown-toggle" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                        Brand
+                # Dropdown for sorting
+                echo "<div class='dropdown'>
+                    <a class='btn btn-light dropdown-toggle' role='button' id='dropdownMenuLink' data-toggle='dropdown' aria-haspopup='true' aria-expanded='false'>
+                        Sort by 
                     </a>
-                    <div class="dropdown-menu" aria-labelledby="dropdownMenuLink">
-                    <a class="dropdown-item" href="../public/session_cart.php?&sort=' . $sort . '">All</a>
-                    <div class="dropdown-divider"></div>';
-                while ($row = mysqli_fetch_array($brands_list, MYSQLI_ASSOC)) {
-                    echo '<a class="dropdown-item" href="../public/session_cart.php?brand=' . $row['item_brand'] . '">' . $row['item_brand'] . '</a>';
-                }
-                echo '</div></div>';
-
-                # Dropdown for types
-                echo '<div class="dropdown mr-5 col">
-                    <a class="btn btn-light dropdown-toggle" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                        Type
-                    </a>
-                    <div class="dropdown-menu" aria-labelledby="dropdownMenuLink">
-                    <a class="dropdown-item" href="../public/session_cart.php?&sort=' . $sort . '">All</a>
-                    <div class="dropdown-divider"></div>';
-                foreach ($types as $type) {
-                    echo '<a class="dropdown-item" href="../public/session_cart.php?type=' . $type . '&sort=' . $sort . '">' . $type . '</a>';
-                }
-                echo '</div></div>
-                </div>
-            </div>';
-
-            # Dropdown for sorting
-            echo '<div class="container d-flex justify-content-end">
-            <div class="dropdown mr-5 col">
-                <a class="btn btn-light dropdown-toggle" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                    Sort by 
-                </a>
-                <div class="dropdown-menu" aria-labelledby="dropdownMenuLink"> 
-                    <a class="dropdown-item" href="../public/session_cart.php?sort=brand' . $filter_query . '">Brand</a>
-                    <a class="dropdown-item" href="../public/session_cart.php?sort=name' . $filter_query . '">Name</a>
-                    <a class="dropdown-item" href="../public/session_cart.php?sort=price-asc' . $filter_query . '">Price: Low to High</a>
-                    <a class="dropdown-item" href="../public/session_cart.php?sort=price-desc' . $filter_query . '">Price: High to Low</a>
-                </div>
-            </div>
-        </div>   
-        </div>';
-
-        # Fetch and display products
-        $q = "SELECT * FROM products $filter ORDER BY $sort";
-        $r = mysqli_query($link, $q);
-        if (!$r) {
-            die("Query failed: " . mysqli_error($link));
-        }
-
-        echo '<div class="row">';
-        if (mysqli_num_rows($r) > 0) {
-            while ($row = mysqli_fetch_array($r, MYSQLI_ASSOC)) {
-                echo '
-                <div class="col-md-3 d-flex justify-content-center mt-4 p-3 text-center">
-                <div class="card" style="width: 18rem;">
-                    <a href="../public/product_page.php?id=' . $row['item_id'] . '">
-                    <img src="../assets/img/' . $row['item_img'] . '.jpg" class="card-img-top" alt="Product Image">
-                    </a>
-                    <div class="card-body">
-                        <h5 class="card-title text-center">' . $row['item_name'] . '</h5>
-                        <h6 class="card-title text-center">by ' . $row['item_brand'] . '</h6>
-                        <p class="card-text">' . $row['item_ml'] . ' ml</p>
+                    <div class='dropdown-menu' aria-labelledby='dropdownMenuLink'> 
+                        <a class='dropdown-item' href='../public/session_cart.php?sort=brand" . $filter_query . "'>Brand</a>
+                        <a class='dropdown-item' href='../public/session_cart.php?sort=name" . $filter_query . "'>Name</a>
+                        <a class='dropdown-item' href='../public/session_cart.php?sort=price-asc" . $filter_query . "'>Price: Low to High</a>
+                        <a class='dropdown-item' href='../public/session_cart.php?sort=price-desc" . $filter_query . "'>Price: High to Low</a>
                     </div>
-                    <ul class="list-group list-group-flush text-center">
-                        <li class="list-group-item"><p class="d-flex justify-content-center align-items-center">£ ' . $row['item_price'] . '</p></li>
-                    </ul>
-                </div>
-            </div>';
+                </div> </div>";
+
+            # Fetch and display products
+            $q = "SELECT * FROM products $filter ORDER BY $sort";
+            $r = mysqli_query($link, $q);
+            if (!$r) {
+                die("Query failed: " . mysqli_error($link));
             }
-        } else {
-            echo '<div class="container-fluid mt-5 text-center">
-                <h4>There are currently no items in the shop.</h4>
-            </div>';
-        }
-        echo '</div>';
+
+            echo '<div class="row">';
+
+            # Product card 
+
+            if (mysqli_num_rows($r) > 0) {
+                while ($row = mysqli_fetch_array($r, MYSQLI_ASSOC)) {
+                    echo '
+                    <div class="col-md-3 d-flex justify-content-center mt-4 p-3 text-center">
+                    <div class="card" style="width: 18rem;">
+                        <a href="../public/product_page.php?id=' . $row['item_id'] . '">
+                        <img src="../assets/img/' . $row['item_img'] . '.jpg" class="card-img-top" alt="Product Image">
+                        </a>
+                        <div class="card-body">
+                            <h5 class="card-title text-center">' . $row['item_name'] . '</h5>
+                            <h6 class="card-title text-center">' . $row['item_brand'] . '</h6>
+                            <p class="card-text font-italic">' . $row['item_ml'] . ' ml</p>
+                        </div>
+                        <ul class="list-group list-group-flush text-center">
+                            <li class="list-group-item"><p class="d-flex justify-content-center align-items-center font-weight-bold">£ ' . $row['item_price'] . '</p></li>
+                        </ul>
+                    </div>
+                </div>';
+                }
+            } else {
+                echo '<div class="container-fluid mt-5 text-center">
+                    <h4>There are currently no items in the shop.</h4>
+                </div>';
+            }
+            echo '</div>
+        </div>';
 
         mysqli_close($link);
         include '../includes/footer.php'; ?>  
